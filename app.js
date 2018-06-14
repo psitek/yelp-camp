d188df86bd2a53312645ac1ff59de0133e4cdda9
Weronika Psitek <weropsitek@gmail.com>
2018-06-14 14:52:44 +0000

    add mongolab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes       = require("./routes/index");    
     
-mongoose.connect("mongodb://localhost/yelp_camp_v13");
+// mongoose.connect("mongodb://localhost/yelp_camp_v13");
+mongoose.connect("mongodb://psitek:Dobrydatabase.214@ds261040.mlab.com:61040/yelpcamp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
